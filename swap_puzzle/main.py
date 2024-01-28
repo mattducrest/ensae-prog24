5a325c6f2234099466e42a680dc170879a713095
matt <matducrest@gmail.com>
2024-01-28 22:14:34 +0100

    Changed path in main.py to absolute path

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -1,10 +1,11 @@
+import os
 from grid import Grid
 
 g = Grid(2, 3)
 print(g)
 
 data_path = "../input/"
-file_name = data_path + "grid0.in"
+file_name = os.path.join('input', 'grid0.in')
 
 print(file_name)
 
