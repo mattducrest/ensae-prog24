e55a9ab8bf498c52db6811173a9dcdb7150f6ba6
aliceisr <alice.israel@ensae.fr>
2024-01-29 09:51:42 +0000

    swap seq

--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -6,6 +6,31 @@ class Solver():
     """
     
     def get_solution(self):
+        for k in range (1, self.n*self.m + 1):
+            i = 0
+            j = 0 
+            v = 0
+            u = 0
+            while state[i][j] != k : 
+                if j < self.n - 1 : 
+                    j=j+1
+                elif j == self.n-1 :
+                    j=0 
+                    i = i + 1 
+
+            while k> self.n : 
+                v = k - self.n 
+            
+            while j != v - 1 : 
+                if j < v-1 : 
+                    self.swap((i,j),(i, j+1))
+                    j = j+1
+                else :
+                    self.swap ((i,j),(i, j-1))
+                    j=j-1
+            
+            
+
         """
         Solves the grid and returns the sequence of swaps at the format 
         [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...]. 
