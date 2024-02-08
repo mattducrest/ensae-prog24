133b1a54f66111019bd611dfbc71cd1666bf7e44
aliceisr <alice.israel@ensae.fr>
2024-02-08 08:46:16 +0000

    solver fini

--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -7,38 +7,28 @@ class Solver():
     """
     A solver class, to be implemented.
     """
-    
-    def get_solution(self):
-        for k in range (1, self.n*self.m + 1):
-            i = 0
-            j = 0 
-            v = 0
-            u = 0
-            cl = 0
-            while state[i][j] != k : 
-                if j < self.n - 1 : 
-                    j=j+1
-                elif j == self.n-1 :
-                    j=0 
-                    i = i + 1 
 
-            
+     # Méthode 2 : algorithme de tri à bulles 
+     
+     def get_solution(self):
+        sequence = []
+        swaps = 0
 
-            # on trouve la ligne sur laquelle le nombre k doit être 
-            while k> self.n : 
-                v = k - self.n 
-                cl += 1 
-                return cl 
-    
-            # on met le nombre k sur la bonne ligne 
-            while i != cl : 
-                if i < cl : 
+        for j in range (0, self.m): 
+            for i in range(0, self.n):
+                if state[i][j] > state[i][j+1] and j < self.m -1 : # on regarde dans l'ordre des cases si elles sont plus grandes que la case à leur droite
+                    self.swap((i,j),(i, j+1)) # si elle est plus grande on swap
+                    sequence.append((i,j),(i, j+1))
+                    swaps = swaps + 1
+                
+                if state[i][j] > state[i+1][j] and i < self.n-1 : # de même on regarde si la case est plus grande que celle diretement en dessous
                     self.swap((i,j),(i+1, j))
-                    i = i+1
-                elif i > cl :
-                    self.swap ((i,j),(i-1, j))
-                    i=i-1
+                    sequence.append((i,j),(i+1, j))
+                    swaps = swpas + 1
 
+        print(sequence) 
+        print(swaps) 
+        return self
 
 
         """
@@ -47,5 +37,5 @@ class Solver():
         """
         # TODO: implement this function (and remove the line "raise NotImplementedError").
         # NOTE: you can add other methods and subclasses as much as necessary. The only thing imposed is the format of the solution returned.
-        raise NotImplementedError
+  
 
