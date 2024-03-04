880fc3c40f70b5bdd1424926d21d9cc39c1b5154
matt <matducrest@gmail.com>
2024-03-04 11:46:39 +0100

    test_bfs fini

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -10,8 +10,9 @@ from graph import Graph
 class Test_BFS(unittest.TestCase):
     def test_graph1(self):
         graph = Graph.graph_from_file("input/graph1.in")
-        resultat_observe = Graph.bfs(graph)
-
+        resultat_observe_int = Graph.bfs(graph)
+        with open("tests/output.txt", "r") as file:
+            resultat_observe = file.read()
 
         with open("input/graph1.path.out", "r") as file:
             resultat_attendu = file.read()
