da6f62f6d7e9a6b2fd48083c1e00c8b0c76d4e54
aliceisr <alice.israel@ensae.fr>
2024-03-05 08:46:42 +0000

    amélioration solver

--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -1,29 +1,20 @@
 from grid import Grid
 import random
 
-
-"La fonction get_solution de cette première classe ne fonctionne pas. L'idée est d'ordonner les coordonnées par lignes et par colonnes."
-"Ceci ne fonctionne pas parce que certaines coordonnées peuvent être 'coincées' à gauche. Par exemple, la fonction dit que la matrice:"
-" [[1,3,4], [2,5,6]] est ordonnée. En effet elle l'est si on ne regarde que les lignes et les colonnes.  Mais on ne peut pas déplacer le 2"
-"Parce qu'il est plus grand que 1 et plus petit que 5, on est donc coincés"
-
-class Solver(): 
-    """
-    A solver class, to be implemented.
-    """
-
-"Cette solution beaucoup plus brutale fonctionne jusqu'a des matrice de taille 3*3. Sur mon pc il faut environ 3 secondes en moyenne pour"
+"Cette solution très brutale fonctionne jusqu'a des matrice de taille 3*3. Sur mon pc il faut environ 3 secondes en moyenne pour"
 "ordonner une matrice 3*3. Pour une matrice 4*4 à part coup de chance majeur il faudrait environ 5 heures. Il est indéniable que cette fonction"
 "solveur fonctionne, mais le temps pour résoudre le problème peut être long pour des grandes matrices"
 
 class SolverRandom(Grid):
+
     def __init__(self, m, n, initial_state=[]):
         super().__init__(m, n, initial_state)
 
+
     def get_solution(self):
         # Perform random swaps until the matrix is sorted
-        sequence = []
-        swaps = 0
+        sequence = [] #liste des swaps effectués
+        swaps = 0 #nombre de swaps
         while not self.is_sorted():
             # Select two random indices for the swap, ensuring no diagonal swaps
             i1, j1 = random.randint(0, self.m - 1), random.randint(0, self.n - 1)
@@ -51,4 +42,40 @@ class SolverRandom(Grid):
         # NOTE: you can add other methods and subclasses as much as necessary. The only thing imposed is the format of the solution returned.
   
 
+
+
+"""
+
+Première méthode que l'on a essayée mais qui ne fonctionne pas : 
+ def get_solution(self):
+        sequence = []
+        swaps = 0
+
+        for i in range (0, self.m): 
+            for j in range(0, self.n): 
+                if  i < self.m-1 and self.state[i][j] > self.state[i+1][j]: # on regarde si la case est plus grande que celle diretement en dessous
+                    self.swap((i,j),(i+1, j)) # si elle est plus grande on swap
+                    sequence.append(((i,j),(i+1, j))) 
+                    swaps = swaps + 1
+
+                if  j < self.n -1 and self.state[i][j] > self.state[i][j+1]: # on regarde dans l'ordre des cases si elles sont plus grandes que la case à leur droite
+                    self.swap((i,j),(i, j+1)) # si elle est plus grande on swap
+                    sequence.append(((i,j),(i, j+1))) # on prend note des deux cellules qu'on a swap
+                    swaps = swaps + 1 # on incrément le compteur du total des swaps
+
+                
+                if self.state[((i+1)*self.n)+1][0] < ((i+1)*self.n+1) and i < self.m-1:
+                   self.swap((((i+1)*self.n)+1),0),(i*self.n,0)
+                   sequence.append((((i+1)*self.n+1,0),(i*self.n+1, 0)))
+                   swaps = swaps + 1 # on incrément le compteur du total des swaps
+
+        print(sequence) 
+        print(f"Total number of swaps: {swaps}") 
+        return self
+
+"La fonction get_solution ne fonctionne pas. L'idée est d'ordonner les coordonnées par lignes et par colonnes."
+"Ceci ne fonctionne pas parce que certaines coordonnées peuvent être 'coincées' à gauche. Par example, la fonction dit que la matrice:"
+" [[1,3,4], [2,5,6]] est ordonnée. En effet elle l'est si on ne regarde que les lignes et les colonnes.  Mais on ne peut pas déplacer le 2"
+"Parce qu'il est plus grand que 1 et plus petit que 5, on est donc coincés"
+""" 
 
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -3,6 +3,7 @@ sys.path.append("swap_puzzle/")
 
 from grid import Grid
 from solver import SolverRandom
+import unittest
 
 """""
 g = Grid.grid_from_file("input/grid1.in")
@@ -14,8 +15,13 @@ Solver.get_solution(g)
 print(Grid.is_sorted(g))
 
 """""
-g1 = Grid.grid_from_file("input/grid2.in")
-print(g1)
-SolverRandom.get_solution(g1)
-print(g1)
 
+class Test_Solver(unittest.TestCase):
+    def test_grid2(self):
+        g1 = Grid.grid_from_file("input/grid2.in")
+        print(g1)
+        SolverRandom.get_solution(g1)
+        print(g1)
+
+if __name__ == '__main__':
+    unittest.main()
