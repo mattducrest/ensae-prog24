10eda70d5ddc69d694e1cd75e636b20ed18e52e4
matt <matducrest@gmail.com>
2024-03-09 13:33:05 +0100

    add new solver function

--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -5,7 +5,7 @@ import random
 "ordonner une matrice 3*3. Pour une matrice 4*4 à part coup de chance majeur il faudrait environ 5 heures. Il est indéniable que cette fonction"
 "solveur fonctionne, mais le temps pour résoudre le problème peut être long pour des grandes matrices"
 
-class SolverRandom(Grid):
+class SolverRandom():
 
     def __init__(self, m, n, initial_state=[]):
         super().__init__(m, n, initial_state)
@@ -41,7 +41,64 @@ class SolverRandom(Grid):
         # TODO: implement this function (and remove the line "raise NotImplementedError").
         # NOTE: you can add other methods and subclasses as much as necessary. The only thing imposed is the format of the solution returned.
   
+sequence_swaps=[] #global variable
+class SolverSearch(): 
+    def __init__(self, m, n, initial_state):
+        self.grid = Grid(m, n, initial_state)
+        self.n = n
+        self.m = m
 
+    def find_coordinates_x(self, grid, x:int):
+        """
+        Get the cell's coordenates (i,j) of an integer
+
+        Parameters: 
+        -----------
+        grid
+            The grid.
+        x: int
+            An integer from the grid.
+
+        Output: 
+        -------
+        (i,j): tuple[int]
+            The integer's cell coordenates.
+        """
+        for i in range(self.m):
+            for j in range(self.n):
+                if grid[i][j] == x: 
+                    return (i, j)
+    
+    def drag_x(self, x):
+        (i, j) = self.find_coordinates_x(self.grid.state, x) # (i,j) coordonnée de x dans la matrice non ordonnée
+        correct_matrix = Grid(self.m, self.n) # Without initial state in order to get a sorted matrix
+        i_target, j_target = self.find_coordinates_x(correct_matrix.state, x)
+        while j != j_target: # On se place dans la meme colonne que la place objectif
+            if j_target < j:
+                for k in range(j - j_target):
+                    sequence_swaps.append(((i, j), (i, j - 1)))
+                    self.grid.swap((i, j), (i, j - 1))
+                    j = j - 1
+            else:
+                for k in range(j_target - j):
+                    sequence_swaps.append(((i, j), (i, j + 1)))
+                    self.grid.swap((i, j), (i, j + 1))
+                    j = j + 1
+        while i != i_target: # On remonte droit vers l'objectif pour ne pas déranger ce qui a été fait
+            for k in range(i - i_target):
+                sequence_swaps.append(((i, j), (i - 1, j)))
+                self.grid.swap((i, j), (i - 1, j))
+                i = i - 1
+
+        current_state = Grid(self.m, self.n, self.grid.state)
+        if current_state != correct_matrix:
+            print(f"Current state after dragging {x}:")
+            print(current_state)
+    
+    def get_solution(self):
+        for x in range(1, self.m * self.n + 1):
+            self.drag_x(x)
+        return sequence_swaps
 
 
 """
---
+++ tests/test_solver_search.py
@@ -0,0 +1,27 @@
+import sys 
+import time
+sys.path.append("swap_puzzle/")
+
+from grid import Grid
+from solver import SolverSearch
+import unittest
+
+class Test_Solver(unittest.TestCase):
+    def test_grid2(self):
+        g1 = Grid.grid_from_file("input/grid2.in")
+        print(g1)
+        m, n = g1.m, g1.n
+        initial_state = g1.state
+        solver = SolverSearch(m, n, initial_state)
+        start_time = time.time()
+        solution = solver.get_solution()
+        print(solver.grid)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"The function took {elapsed_time} seconds to run.")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+    sys.path.append("swap_puzzle/")
